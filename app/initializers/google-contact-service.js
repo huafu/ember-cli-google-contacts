aae269675668928964052ba8feb7d52a2b5f4944
Tim Greiser <tgreiser@gmail.com>
2015-10-03 08:06:32 -0700

    bugfix - correct initializers so it will deferReadiness

--- app/initializers/google-contact-service.js
+++ app/initializers/google-contact-service.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
 
 export function initialize(registry, application) {
   application.inject('route', 'googleContactService', 'service:google-contact');
   application.inject('controller', 'googleContactService', 'service:google-contact');
   application.inject('adapter:google-contact', 'googleContactService', 'service:google-contact');
   application.inject('model:google-contact', 'googleContactService', 'service:google-contact');
+ 
+  if (Ember.getWithDefault(ENV, 'social.google.autoLoad', true)) {
+    application.deferReadiness();
+    application.__container__.lookup('service:google-contact').load().then(function() {
+      application.advanceReadiness();
+    });
+  }
 }
 
 export default {
